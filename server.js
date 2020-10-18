ff74a5ef07893077cee07996d2eb56807a64f3b7
irrako <irraquex@gmail.com>
2020-10-18 16:20:26 -0400

    prac5

--- server.js
+++ server.js
@@ -3,51 +3,45 @@ http = require('http'),
 app = express(),
 server = http.createServer(app),
 io = require('socket.io').listen(server);
+
 app.get('/', (req, res) => {
-
-res.send('Chat Server is running on port 3000')
+res.send('Chat Server is running on port 8000')
 });
+
 io.on('connection', (socket) => {
+ console.log('user connected')
 
-console.log('user connected')
+ socket.on('join', function(userNickname) {
 
-socket.on('join', function(userNickname) {
+       console.log(userNickname +" : has joined the chat "  );
 
-        console.log(userNickname +" : has joined the chat "  );
-
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+       socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
     });
 
-
-socket.on('messagedetection', (senderNickname,messageContent) => {
+ socket.on('messagedetection', (senderNickname,messageContent) => {
        
-       //log the message in console 
-
+       //Registrar el mensaje en la consola 
        console.log(senderNickname+" :" +messageContent)
-        //create a message object 
+
+       //Crear un objeto de mensaje
        let  message = {"message":messageContent, "senderNickname":senderNickname}
-          // send the message to the client side  
+
+       //Enviar el mensaje al lado del cliente
        io.emit('message', message );
      
-      });
+    });
       
   
  socket.on('disconnect', function() {
-    console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") 
+       console.log( ' user has left ')
+       socket.broadcast.emit("userdisconnect"," user has left ") 
+
+   });
 
 });
 
+server.listen(8000,()=>{
 
-
-});
-
-
-
-
-
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000');
+console.log('Node app is running on port 8000');
 
 });
